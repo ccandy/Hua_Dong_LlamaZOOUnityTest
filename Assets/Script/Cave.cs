913cc6a1347db175cd859502599afcdfdb0399b7
Andy Dong <Andy@Ans-iMac.local>
2016-04-08 10:46:30 -0700

    add missing files

---
+++ Assets/Script/Cave.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cave : MonoBehaviour {
+
+	// Use this for initialization
+
+	public bool trapCave;
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
